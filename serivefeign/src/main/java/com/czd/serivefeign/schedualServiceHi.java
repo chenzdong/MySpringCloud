f6f5596cab4ac4baa77403b99ec0b246dd78bde0
chenzd <chenzd@wasu.com>
2018-03-16 16:44:28 +0800

    添加feign工程

---
+++ serivefeign/src/main/java/com/czd/serivefeign/schedualServiceHi.java
@@ -0,0 +1,19 @@
+package com.czd.serivefeign;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * feign调取的服务接口
+ *
+ * @author: czd
+ * @create: 2018/3/16 16:30
+ */
+@FeignClient(value="service-hi")
+public interface SchedualServiceHi {
+    @RequestMapping(value="/hi",method = RequestMethod.GET)
+    String sayHi(@RequestParam(value = "name") String param);
+}
